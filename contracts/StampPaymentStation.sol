6a487f5c66a3877017d29da188dae4b1d776a820
FarajiOranj <farajioranj@gmail.com>
2024-11-07 13:20:06 +0330

    under develop

--- contracts/StampPaymentStation.sol
+++ contracts/StampPaymentStation.sol
@@ -2,10 +2,23 @@
 pragma solidity 0.8.25;
 
 contract StampPaymentStation {
-    struct PaymentInfo {
-        
-        uint256 spent;
+    enum PaymentToken {
+        USDT,
+        USDC
     }
+    mapping(address => uint256) public chainNonce;
+    mapping(bytes32 => bool) public hashed;
 
-    mapping(bytes32 => uint256) public hashToPaymentInfo;
+    function pay(
+        PaymentToken paymentToken,
+        PaymentToken destinationToken,
+        uint256 amount,
+        bytes32 hash,
+        bytes32 sigHash
+    ) external {
+        require(!hashed[hash], "SPS: HASH_REGISTERED_BEFORE");
+        
+
+        chainNonce[msg.sender]++;
+    }
 }
