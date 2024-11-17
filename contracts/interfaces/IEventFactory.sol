0e0134492f16aa9947ee9ba954758322e96ec357
FarajiOranj <farajioranj@gmail.com>
2024-11-17 13:55:44 +0330

    feat: IEventFactory Created and createEventFunc interface added

---
+++ contracts/interfaces/IEventFactory.sol
@@ -0,0 +1,33 @@
+//  SPDX-License-Identifier: -- STAMP --
+pragma solidity 0.8.25;
+
+enum Transferable {
+    True,
+    False,
+    Conditional
+}
+
+struct EventInfo {
+    bool Virtual;
+    Transferable Transferable;
+    uint8 Type; // Private Event, Public Event, Approval Event, and more for future...
+    uint8 Limit;
+    uint64 UTCtime;
+    address Creator;
+    uint128 Price;
+    uint128 TotalSupply;
+    bytes32 LocationHash;
+    string Name;
+    string Description;
+    string[] Tags;
+}
+
+interface IEventFactory {
+    event EventCreated(EventInfo eventInfo);
+
+    function createFactory(
+        EventInfo calldata eventInfo,
+        uint256 deadline,
+        bytes calldata signature
+    ) external;
+}
