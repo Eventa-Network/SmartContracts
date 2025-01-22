350b75bdcf95fccffaa176dc170efed7098baffe
FarajiOranj <farajioranj@gmail.com>
2025-01-22 11:21:54 +0330

    fix: contract name changed to GroveEvent

--- contracts/GroveEvent.sol
+++ contracts/GroveEvent.sol
@@ -1,4 +1,4 @@
-//  SPDX-License-Identifier: -- STAMP --
+//  SPDX-License-Identifier: -- Grove --
 pragma solidity 0.8.25;
 
 import {IGroveEventFactory} from "./interfaces/IGroveEventFactory.sol";
