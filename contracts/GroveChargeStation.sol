6cd98ce05857238549b61cb15e1b16ce4eaab77e
FarajiOranj <farajioranj@gmail.com>
2025-02-27 14:06:09 +0330

    feat & fix: UserTX added for better tracking

--- contracts/GroveChargeStation.sol
+++ contracts/GroveChargeStation.sol
@@ -5,10 +5,21 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IUSDCswapper} from "./interfaces/IUSDCswapper.sol";
 
 contract GroveChargeStation {
+    struct UserTX {
+        address user;
+        address platform;
+        address token;
+        uint256 tokenAmount;
+        uint256 amountInUSDC;
+        bytes32 txID;
+    }
+
     IERC20 public immutable USDC;
     IUSDCswapper public immutable USDC_SWAPPER;
 
+    UserTX[] public usersTXs;
     mapping(address => uint256) public platformUSDCbalance;
+    mapping(address => uint256) public userNonce;
 
     event Charged(
         address indexed chargedFor,
@@ -67,6 +78,17 @@ contract GroveChargeStation {
         }
 
         platformUSDCbalance[platform] += amountInUSDC;
+        usersTXs.push(
+            UserTX(
+                for_,
+                platform,
+                token,
+                amount,
+                amountInUSDC,
+                (keccak256(abi.encodePacked(for_, userNonce[for_])))
+            )
+        );
+        userNonce[for_]++;
     }
 
     function transferUSDC(address to, uint256 amount) external {
@@ -87,5 +109,76 @@ contract GroveChargeStation {
             msg.sender,
             platformUSDCbalance[msg.sender]
         );
+    }
+
+    function paginatedUsersTXs(uint256 page)
+        external
+        view
+        returns (
+            uint256 currentPage,
+            uint256 totalPages,
+            UserTX[] memory paggedArray
+        )
+    {
+        if (usersTXs.length == 0)
+            return (currentPage, totalPages, paggedArray);
+        else if (usersTXs.length < 11) {
+            paggedArray = new UserTX[](usersTXs.length);
+
+            uint256 x;
+            while (true) {
+                paggedArray[x] = usersTXs[
+                    usersTXs.length - 1 - x
+                ];
+
+                if (x == usersTXs.length - 1) break;
+
+                unchecked {
+                    x++;
+                }
+            }
+
+            return (1, 1, paggedArray);
+        }
+
+        if (page == 0) page = 1;
+
+        totalPages = usersTXs.length / 10;
+
+        uint256 diffLength = usersTXs.length -
+            (totalPages * 10);
+
+        if (totalPages * 10 < usersTXs.length) totalPages++;
+        if (page > totalPages) page = totalPages;
+        currentPage = page;
+
+        uint256 firstIndex;
+        uint256 lastIndex;
+        if (page == 1) {
+            firstIndex = usersTXs.length - 1;
+            lastIndex = firstIndex - 10;
+        } else if (page == totalPages)
+            firstIndex = diffLength == 0 ? firstIndex = 9 : diffLength - 1;
+        else {
+            firstIndex +=
+                ((totalPages - page) * 10) +
+                (diffLength != 0 ? diffLength - 1 : 0);
+            lastIndex +=
+                ((totalPages - page - 1) * 10) +
+                (diffLength != 0 ? diffLength - 1 : 0);
+        }
+
+        paggedArray = new UserTX[]((firstIndex + 1) - lastIndex);
+
+        uint256 i;
+        while (true) {
+            paggedArray[i] = usersTXs[firstIndex];
+
+            if (firstIndex == lastIndex) break;
+            unchecked {
+                i++;
+                firstIndex--;
+            }
+        }
     }
 }
